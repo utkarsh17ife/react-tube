bce03a41f46eb6227ca6f48e31c9059520cb40a9
Utkarsh Tyagi <Utkarsh.Tyagi@mea.ey.net>
2018-01-07 10:12:20 +0530

    inital commit

---
+++ README.md
@@ -0,0 +1,8 @@
+In progress
+
+
+
+> git clone https://github.com/utkarsh17ife/react-tube.git
+> cd react-tube
+> npm install
+> npm start
---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import SearchBar from './search_bar';
+
+const API_KEY = 'AIzaSyDLJ5g8779ED38FyAyRkmrkkpYcu';
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Hi this is youtube</h1>
+      <SearchBar />
+    </div>
+  )
+
+}
+
+export default App;
