14320fcee24575a9e73a04e144db4f5a0518f329
Utkarsh Tyagi <Utkarsh.Tyagi@mea.ey.net>
2018-01-20 13:49:16 +0530

    added node

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
+const path = require('path');
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname + '/public/js'),
+    publicPath: "/js/",
+    filename: 'bundle.js',
   },
   module: {
     loaders: [{
